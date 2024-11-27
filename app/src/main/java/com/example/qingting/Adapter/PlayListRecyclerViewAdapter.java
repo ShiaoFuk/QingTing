c57017f99e7f82d8992c7d58bad21c6e61ac806d
Rabit <msf1090480147@outlook.com>
2024-11-28 02:08:56 +0800

    add ensure dialog before delete a playlist. add a plaintext dialog in DialogUtils which don't contain a edittext.

--- app/src/main/java/com/example/qingting/Adapter/PlayListRecyclerViewAdapter.java
+++ app/src/main/java/com/example/qingting/Adapter/PlayListRecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package com.example.qingting.Adapter;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +13,16 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.qingting.Bean.PlayList;
+import com.example.qingting.MainActivity;
 import com.example.qingting.MyApplication;
 import com.example.qingting.R;
+import com.example.qingting.Utils.DialogUtils;
 import com.example.qingting.Utils.ImageLoadUtils;
 import com.example.qingting.Utils.ToastUtils;
 import com.example.qingting.data.DB.PlayListDB;
 import com.example.qingting.data.SP.LoginSP;
 import com.example.qingting.net.request.PlayListRequest.DeletePlayListRequest;
+import com.example.qingting.net.request.PlayListRequest.UpdatePlayListRequest;
 import com.example.qingting.net.request.RequestListener;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -26,6 +30,7 @@ import com.google.gson.JsonObject;
 import java.util.List;
 
 public class PlayListRecyclerViewAdapter extends RecyclerView.Adapter<PlayListRecyclerViewViewHolder> {
+    final static String TAG = PlayListRecyclerViewAdapter.class.getName();
     List<PlayList> playListList;
 
     public PlayListRecyclerViewAdapter(List<PlayList> playListList) {
@@ -42,6 +47,63 @@ public class PlayListRecyclerViewAdapter extends RecyclerView.Adapter<PlayListRe
     @Override
     public void onBindViewHolder(@NonNull PlayListRecyclerViewViewHolder holder, int position) {
         final PlayList playList = playListList.get(position);
+        holder.view.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                DialogUtils.showInputDialog(holder.view.getContext(),
+                        holder.view.getResources().getString(R.string.update_playlist_title),
+                        holder.view.getResources().getString(R.string.update_playlist_message),
+                        holder.view.getResources().getString(R.string.sure_message),
+                        new DialogUtils.DialogCallback() {
+                            @Override
+                            public void doSth(View view, String input) {
+                                UpdatePlayListRequest.deletePlayList(new RequestListener() {
+                                    @Override
+                                    public Object onPrepare(Object object) {
+                                        return null;
+                                    }
+
+                                    @Override
+                                    public void onRequest() {
+
+                                    }
+
+                                    @Override
+                                    public void onReceive() {
+
+                                    }
+
+                                    @Override
+                                    public void onSuccess(JsonElement element) throws Exception {
+                                        if (element.getAsJsonObject().get("code").getAsInt() == 200) {
+                                            final Handler handler = new Handler(Looper.getMainLooper());
+                                            MyApplication.getPlayListFromNet(holder.view.getContext());
+                                            handler.post(()->{
+                                                // 更新成功
+                                                playListList.get(holder.getAdapterPosition()).setName(input);
+                                                notifyItemChanged(holder.getAdapterPosition());
+                                                ToastUtils.makeShortText(holder.view.getContext(), holder.view.getResources().getString(R.string.update_playlist_success));
+                                            });
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onError(Exception e) {
+                                        ToastUtils.makeShortText(holder.view.getContext(), holder.view.getResources().getString(R.string.update_playlist_fail));
+                                        Log.e(TAG, e.getMessage());
+                                    }
+
+                                    @Override
+                                    public void onFinish() {
+
+                                    }
+                                }, LoginSP.getToken(holder.view.getContext()), playList.getId(), input);
+                            }
+                        }, holder.view
+                );
+                return false;
+            }
+        });
         if (playList.getUrl() != null) {
             if (playList.getUrl().startsWith("http")) {
                 ImageLoadUtils.loadFromNet(holder.imageView, playList.getUrl());
@@ -51,8 +113,8 @@ public class PlayListRecyclerViewAdapter extends RecyclerView.Adapter<PlayListRe
         } else {
             holder.imageView.setImageResource(ImageLoadUtils.DEFAULT_SRC_ID);
         }
-        holder.songName.setText(playList.getName());
-        holder.songInfo.setText(playList.getPlayTimes() + "播放 · " + playList.getLikes() + "点赞");
+        holder.playListName.setText(playList.getName());
+        holder.playListInfo.setText(playList.getPlayTimes() + "播放 · " + playList.getLikes() + "点赞");
         holder.playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,46 +125,55 @@ public class PlayListRecyclerViewAdapter extends RecyclerView.Adapter<PlayListRe
         holder.deleteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DeletePlayListRequest.deletePlayList(new RequestListener() {
-                    @Override
-                    public Object onPrepare(Object object) {
-                        return null;
-                    }
+                DialogUtils.showEnsureDialog(holder.deleteBtn.getContext(),
+                        holder.deleteBtn.getContext().getString(R.string.delete_playlist_title),
+                        holder.deleteBtn.getContext().getString(R.string.delete_playlist_message),
+                        holder.deleteBtn.getContext().getString(R.string.sure_message),
+                        new DialogUtils.DialogCallback() {
+                            @Override
+                            public void doSth(View view, String input) {
+                                DeletePlayListRequest.deletePlayList(new RequestListener() {
+                                    @Override
+                                    public Object onPrepare(Object object) {
+                                        return null;
+                                    }
 
-                    @Override
-                    public void onRequest() {
+                                    @Override
+                                    public void onRequest() {
 
-                    }
+                                    }
 
-                    @Override
-                    public void onReceive() {
+                                    @Override
+                                    public void onReceive() {
 
-                    }
+                                    }
 
-                    @Override
-                    public void onSuccess(JsonElement element) throws Exception {
-                        JsonObject jsonObject = element.getAsJsonObject();
-                        if (jsonObject.get("code") != null && jsonObject.get("code").getAsInt() == 200) {
-                            PlayListDB.deletePlayList(v.getContext(), playList);
-                            ToastUtils.makeShortText(v.getContext(), v.getResources().getString(R.string.operate_playlist_success));
-                            MyApplication.deletePlayList(playList);
-                            final Handler handler = new android.os.Handler(Looper.getMainLooper());
-                            handler.post(()->{
-                                notifyDataSetChanged();
-                            });
-                        }
-                    }
+                                    @Override
+                                    public void onSuccess(JsonElement element) throws Exception {
+                                        JsonObject jsonObject = element.getAsJsonObject();
+                                        if (jsonObject.get("code") != null && jsonObject.get("code").getAsInt() == 200) {
+                                            PlayListDB.deletePlayList(v.getContext(), playList);
+                                            ToastUtils.makeShortText(v.getContext(), v.getResources().getString(R.string.operate_playlist_success));
+                                            MyApplication.deletePlayList(playList);
+                                            final Handler handler = new android.os.Handler(Looper.getMainLooper());
+                                            handler.post(() -> {
+                                                notifyDataSetChanged();
+                                            });
+                                        }
+                                    }
 
-                    @Override
-                    public void onError(Exception e) {
-                        ToastUtils.makeShortText(v.getContext(), v.getResources().getString(R.string.operate_playlist_fail));
-                    }
+                                    @Override
+                                    public void onError(Exception e) {
+                                        ToastUtils.makeShortText(v.getContext(), v.getResources().getString(R.string.operate_playlist_fail));
+                                    }
 
-                    @Override
-                    public void onFinish() {
+                                    @Override
+                                    public void onFinish() {
 
-                    }
-                }, LoginSP.getToken(v.getContext()), playList.getId());
+                                    }
+                                }, LoginSP.getToken(v.getContext()), playList.getId());
+                            }
+                        }, null);
             }
         });
     }
@@ -124,16 +195,16 @@ public class PlayListRecyclerViewAdapter extends RecyclerView.Adapter<PlayListRe
 class PlayListRecyclerViewViewHolder extends RecyclerView.ViewHolder {
     View view;
     ImageView imageView;
-    TextView songName;
-    TextView songInfo;
+    TextView playListName;
+    TextView playListInfo;
     ImageView playBtn;
     ImageView deleteBtn;
     public PlayListRecyclerViewViewHolder(@NonNull View itemView) {
         super(itemView);
         this.view = itemView;
         this.imageView = itemView.findViewById(R.id.play_list_img);
-        this.songName = itemView.findViewById(R.id.play_list_name);
-        this.songInfo = itemView.findViewById(R.id.play_list_info);
+        this.playListName = itemView.findViewById(R.id.play_list_name);
+        this.playListInfo = itemView.findViewById(R.id.play_list_info);
         this.playBtn = itemView.findViewById(R.id.play_btn);
         this.deleteBtn = itemView.findViewById(R.id.delete_btn);
     }
--- app/src/main/java/com/example/qingting/Utils/DialogUtils.java
+++ app/src/main/java/com/example/qingting/Utils/DialogUtils.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.qingting.R;
 
@@ -16,6 +17,17 @@ public class DialogUtils {
     public static interface DialogCallback {
         public void doSth(View view, String input);
     }
+
+
+    /**
+     * 带输入的确认框
+     * @param context
+     * @param title
+     * @param message
+     * @param sureMessage
+     * @param dialogCallback
+     * @param view
+     */
     public static void showInputDialog(Context context, String title, String message, String sureMessage, DialogCallback dialogCallback, View view) {
         // 创建 EditText 输入框
         final EditText input;
@@ -50,4 +62,45 @@ public class DialogUtils {
             window.setIcon(R.drawable.app_icon);
         }
     }
+
+
+    /**
+     * 确认框
+     * @param context
+     * @param title
+     * @param message
+     * @param sureMessage
+     * @param dialogCallback
+     * @param view
+     */
+    public static void showEnsureDialog(Context context, String title, String message, String sureMessage, DialogCallback dialogCallback, View view) {
+        // 创建对话框
+        AlertDialog dialog = new AlertDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(sureMessage, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 处理输入内容
+                        Activity activity = (Activity) context;
+                        activity.runOnUiThread(()->{
+                            dialogCallback.doSth(view, null);
+                        });
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel(); // 取消对话框
+                    }
+                })
+                .show(); // 显示对话框
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            Window window = dialog.getWindow();
+            window.setBackgroundBlurRadius(32);
+            window.setBackgroundDrawableResource(R.drawable.dialog_background_shape);
+            window.setIcon(R.drawable.app_icon);
+        }
+    }
+
 }
